8cc502409d78afd62263e02fa7019b6949a75f5a
adrianburgos <adrian.burgos.colas@gmail.com>
2021-10-30 14:43:37 -0400

    borrado de las imprementaciones obsoletas

--- src/App.js
+++ src/App.js
@@ -9,64 +9,29 @@ function App() {
   const [sessionStart,setSessionStart] = useState(false);
   const [timer,setTimer] = useState({session:25,break:5});
   const [sessionSelector,setSelector] = useState(true);
-  const [timerInit,setTimerInit] = useState({session:false,break:false});
-  // const [timeLeft,setTimeLeft] = useState('25:00');
-  
 
   useEffect(()=>{
-    
-    // if(sessionSelector){
-      
-    //   setTimeLeft(timerFormat(timer.session));
-    // }else{
-      
-    //   setTimeLeft(timerFormat(timer.break));
-    // }
     let timeOut = setTimeout(()=>{
       if(sessionStart){
-        // if(timerInit.session){
-        //   setTimer((state)=>{ return {...state,session:sessionLength+1}});
-        //   // setTimeLeft(timerFormat(sessionLength));
-        //   setTimerInit((state)=>{ return {...state,session:false}});
-        // }
-        // if(timerInit.break){
-        //   setTimer((state)=>{ return {...state,break:breakLength+1}});
-        //   // setTimeLeft(timerFormat(breakLength));
-        //   setTimerInit((state)=>{ return {...state,break:false}});
-        // }
-        
         if(sessionSelector){
-          
           if(timer.session > 0){
             setTimer((state)=>{ return {...state,session:state.session-1}});
-            // setTimeLeft(timerFormat(timer.session));
           }else{
             setSelector((state)=>!state);
             setTimer((state)=>{ return {...state,session:sessionLength}});
-            // setTimeLeft(timerFormat(sessionLength));
           }
-          
         }else{
-          // setTimeLeft(timerFormat(timer.break));
           if(timer.break > 0){
             setTimer((state)=>{ return {...state,break:state.break-1}})
-            // setTimeLeft(timerFormat(timer.break));
           }else{
             setSelector((state)=>!state)
             setTimer((state)=>{ return {...state,break:breakLength}})
-            // setTimeLeft(timerFormat(breakLength));
           }
-          
         }
       }
-      
     },1000);
-    // console.log('timer '+timer.session+' '+timer.break);
-    // console.log(sessionLength)
-    // console.log(breakLength)
     return ()=>clearTimeout(timeOut);
   });
-
   return (
     <div className="App">
       <header className="App-header">
@@ -78,7 +43,6 @@ function App() {
       <div id="break-length">{breakLength}</div>
       <button id="break-decrement" onClick={()=>{
         if(!sessionStart){
-          // setTimerInit((state)=>{ return {...state,break:true}})
           setBreak((state)=>{
             if(state>1){
               setTimer((timer)=>{ return {...timer,break:state-1}})
@@ -91,7 +55,6 @@ function App() {
       }}>-</button>
       <button id="break-increment" onClick={()=>{
         if(!sessionStart){
-          // setTimerInit((state)=>{ return {...state,break:true}})
           setBreak((state)=>{
             if(state<60){
               setTimer((timer)=>{ return {...timer,break:state+1}})
@@ -108,7 +71,6 @@ function App() {
       <div id="session-length">{sessionLength}</div>
       <button id="session-decrement" onClick={()=>{
         if(!sessionStart){
-          // setTimerInit((state)=>{ return {...state,session:true}})
           setSession((state)=>{
             if(state>1){
               setTimer((timer)=>{ return {...timer,session:state-1}})
@@ -117,12 +79,10 @@ function App() {
               return state;
             }
           })
-          // setTimer((state)=>{ return {...state,session:111}})
         }
       }}>-</button>
       <button id="session-increment" onClick={()=>{
         if(!sessionStart){
-          // setTimerInit((state)=>{ return {...state,session:true}})
           setSession((state)=>{
             if(state<60){
               setTimer((timer)=>{ return {...timer,session:state+1}})
@@ -141,12 +101,6 @@ function App() {
       {/* START/STOP */}
       <button id="start_stop" onClick={()=>{
         setSessionStart((state)=>!state);
-        // if(timer.session === 111){
-        //   setTimer((state)=>{ return {...state,session:sessionLength}})
-        // }
-        // if(timer.break === 111){
-        //   setTimer((state)=>{ return {...state,break:breakLength}})
-        // }
       }}>{sessionStart?'Pause':'Start'}</button>
 
       {/* RESET */}
@@ -156,7 +110,6 @@ function App() {
         setSessionStart(false);
         setTimer({session:25,break:5})
         setSelector(true);
-        // setTimeLeft('25:00');
       }}>reset</button>
       <footer>
 
